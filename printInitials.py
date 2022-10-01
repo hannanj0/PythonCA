d86f3ddd68c997b63169c3542fa4a629a3293400
hannanj0 <hannanjawad@hotmail.com>
2022-10-01 14:24:36 +0100

    firstProject

---
+++ printInitials.py
@@ -0,0 +1,15 @@
+# A program that prints my initials and displays it in block letters
+# Each block letter is 7x5 and formed by the letter itself
+
+# This example uses multi-line string to print out the block letters
+
+initials_hj = """
+H   H   JJJJJ
+H   H     J
+H   H     J
+HHHHH     J
+H   H     J
+H   H     J
+H   H   JJJ
+"""
+print(initials_hj)
