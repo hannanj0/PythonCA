848c4b60ecf773ee65ace75116d54e682e2c2384
hannanj0 <hannanjawad@hotmail.com>
2022-12-17 18:28:30 +0000

    dictionaries

---
+++ songs.py
@@ -0,0 +1,21 @@
+songs = ["Like a Rolling Stone", "Satisfaction", "Imagine",
+         "What's Going On", "Respect", "Good Vibrations"]
+playcounts = [78, 29, 44, 21, 89, 5]
+
+# Zip the two lists songs and playcounts
+zipped_plays = zip(songs, playcounts)
+
+# Using dict comprehension
+plays = {song: playcount for [song, playcount] in zipped_plays}
+
+# Adding a new entry to plays
+plays["Respect"] = 94
+
+# Changing an entry that already exists in plays
+plays["Purple Haze"] = 1
+
+# Creating a dictionary called library, consisting of value plays and empty value
+library = {"The Best Songs": plays, "Sunday Feelings": {}}
+
+# Printing library to the console
+print(library)
